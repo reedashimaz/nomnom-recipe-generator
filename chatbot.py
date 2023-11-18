d994598ec716ebf941c1a92fac5913f9a9bf6bf9
Reeda Shimaz Huda <133261457+reedashimaz@users.noreply.github.com>
2023-11-19 02:52:28 +0300

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -9,19 +9,23 @@ openai_api_key = os.getenv('OPENAI_API_KEY')
 
 st.title("💬 NomNom Bot")
 st.caption("🚀 Your personal recipe generator!")
+st.caption("Enter a cuisine and your dietary restrictions to get started :)")
+
+with st.sidebar:
+    cuisine = st.text_input("Cuisine you're craving", key="cuisine")
+    diet_res = st.text_input("Dietary Restrictions", key = "diet_res")
 
 ingredients = {
     'Tomatoes' : 3,
-    'Pasta' : 1
+    'Rice' : 1
 }
-cuisine = 'Italian'
-diet_res = 'Vegetarian'
 
 content_template = """
 I have scanned/entered my grocery store receipts. Here are the items and their quantity (in the form of a dictionary) I have purchased: 
 {ingredients}.
 My dietary restrictions include {diet_res}. I am in the mood for {cuisine} recipes. Give me a detailed recipe using ONLY
-the ingredients I have. I also have common pantry items like salt, pepper, olive oil, and basic spices. Start your response with
+the ingredients I have. I also have common pantry items like salt, pepper, olive oil, and basic spices. Make sure to take the 
+cuisine and dietary restructions into consideration definitively. Start your response with
 "Howdy, I'm the NomNom Bot!" and end by asking if the user has any follow-up questions or needs any additional resources.
 """
 
@@ -35,20 +39,21 @@ for msg in st.session_state.messages[1:]:
     st.chat_message(msg["role"]).write(msg["content"])
 
 # Generate a response for the initial user message
-if len(st.session_state.messages) == 1:
-    client = OpenAI(api_key=openai_api_key)
-    response = client.chat.completions.create(
-        model="gpt-3.5-turbo", 
-        messages=st.session_state.messages,
-        max_tokens=2048,
-        n=1,
-        stop=None,
-        temperature=0.5
-    )
+if cuisine and diet_res:
+    if len(st.session_state.messages) == 1:
+        client = OpenAI(api_key=openai_api_key)
+        response = client.chat.completions.create(
+            model="gpt-3.5-turbo", 
+            messages=st.session_state.messages,
+            max_tokens=2048,
+            n=1,
+            stop=None,
+            temperature=0.5
+        )
 
-    msg = response.choices[0].message.content
-    st.session_state.messages.append({"role": "assistant", "content": msg})
-    st.chat_message("assistant").write(msg)
+        msg = response.choices[0].message.content
+        st.session_state.messages.append({"role": "assistant", "content": msg})
+        st.chat_message("assistant").write(msg)
 
 # User can ask follow-up questions
 if prompt := st.chat_input():
