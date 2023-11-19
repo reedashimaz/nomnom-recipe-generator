c9ff8bf4022f40c7e9a0a6b54b5f01cd17cdb1ac
Vivek Atmuri <vatmuri04@gmail.com>
2023-11-19 03:40:14 -0500

    Final Commit with Functionaloty

--- intro.py
+++ intro.py
@@ -16,6 +16,11 @@ import os
 
 from dotenv import load_dotenv, find_dotenv
 
+st.set_page_config(
+    page_title="Intro",
+    layout='wide'
+)
+
 # Load environment variables
 _ = load_dotenv(find_dotenv())
 
@@ -40,7 +45,6 @@ def upload_receipt():
         st.success("Receipt uploaded successfully!")
         # Add your code here for processing the uploaded receipt
 
-st.set_page_config(layout="wide")
 
 
 if 'clicked' not in st.session_state:
@@ -108,20 +112,23 @@ ingredient_quantities = {}
 # Initialize or retrieve the existing dataframe to store grocery items
 #grocery_data = initialize_grocery_data()
 
-tab1, tab2, tab3 = st.tabs(["Add Groceries", "Available Groceries", "Create recipes"])
+tab1, tab2 = st.tabs(["Add Groceries", "Available Groceries"])
 
 df = pd.DataFrame()
  
 with tab1:
     st.button("Switch to OCR Input", on_click=switch_style)
-    col1, col2 = st.columns([2, 1])
+    col1, col2 = st.columns([1.5, 1])
     if session_state.manual_entry:
         with col1:
             if not session_state.clicked:
                 st.subheader("Add Groceries - Manually Input Items")
                 session_state.grocery_data = st.data_editor(pd.DataFrame(columns=['Name', 'Quantity', 'Additional Notes/Expiration Date']), key=session_state.editor_key, num_rows="dynamic", hide_index=True, use_container_width=True)
+                print(session_state.grocery_data)
             else:
                 session_state.editor_key += 1
+
+            st.button("Press me to update Tab 2", on_click=click_save)
 
 
 
@@ -162,7 +169,7 @@ with tab1:
                     text_list.append(pred_text)
                     confidence_list.append(pred_confidence)
 
-                df = pd.DataFrame({'Text': text_list, 'Confidence': confidence_list})
+                df = pd.DataFrame({'Text': text_list})
 
         with col2:
             if session_state.file_uploaded:
@@ -175,10 +182,9 @@ with tab1:
                     help="Select if you want to add this to the list.",
                     default=False
                 )}, key="OCR_DataFrame")
-                st.write(session_state["OCR_DataFrame"])
                 session_state.ocr_extracted_values = df2
 
-    st.button("Press me to update Tab 2", on_click=click_save)
+            st.button("Press me to update Tab 2", on_click=click_save)
 
 
 
@@ -252,100 +258,7 @@ with tab2:
         list_display(st.session_state.total_groceries)
 
 
-with tab3:
-    
-    # # Create columns for layout
-    # col1, col2 = st.columns([0.20, 0.80])
-
-    # # Column 1: Display the embedded video without controls
-    # with col1:
-    #     video_html = f"""
-    #     <video width="100%" height="auto" autoplay muted style="margin-top: -30px;">
-    #       <source src="{video_url}" type="video/mp4">
-    #       Your browser does not support the video tag.
-    #     </video>
-    #     """
-    #     st.markdown(video_html, unsafe_allow_html=True)
-
-    # with col2:
-    #     st.header("NomNom Bot")
-    #     st.markdown('<style>div.Widget.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
-
-    # st.caption("🍳 Your personal recipe generator!")
-    # st.caption("Enter a cuisine and your dietary restrictions to get started :)")
-
-    # with st.sidebar:
-    #     cuisine = st.text_input("Cuisine you're craving", key="cuisine")
-    #     diet_res = st.text_input("Dietary Restrictions", key="diet_res")
-
-    # ingredients = {
-    #     'Tomatoes': 3,
-    #     'Rice': 1
-    # }
-
-    # content_template = """
-    # I have scanned/entered my grocery store receipts. Here are the items and their quantity (in the form of a dictionary) I have purchased: 
-    # {ingredients}.
-    # My dietary restrictions include {diet_res}. I am in the mood for {cuisine} recipes. Give me a detailed recipe using ONLY
-    # the ingredients I have. I also have common pantry items like salt, pepper, olive oil, and basic spices. 
-    # Make sure to take the cuisine and dietary restrictions into consideration definitively. 
-    # Provide a recipe that aligns with the preferred cuisine given by the user. A cuisine is a style or method of cooking, especially of a particular country
-    # or region. Use ingredients, foods, and techniques from the cuisine from that specific region.
-    # If the user says their dietary restriction is an X allergy or allergic to X where X is any ingredient, make sure to exclude X from the recipe
-    # since the user could die of an allergic reaction and you will be responsible.
-    # Start your response with "Howdy, I'm the NomNom Bot!" and end by asking if the user has any follow-up questions or needs any additional resources.
-    # """
-    # print("Content Template:", content_template.format(ingredients=ingredients, diet_res=diet_res, cuisine=cuisine))
-    # # Initialize the chat with an initial user message
-    # if cuisine and diet_res:
-    #     if "messages" not in st.session_state:
-    #         print("Content Template:", content_template.format(ingredients=ingredients, diet_res=diet_res, cuisine=cuisine))
-    #         st.session_state["messages"] = [
-    #             {"role": "user", "content": content_template.format(ingredients=ingredients, diet_res=diet_res, cuisine=cuisine)}]
-
-    #     # Display the chat messages
-    #     for msg in st.session_state.messages[1:]:
-    #         st.chat_message(msg["role"]).write(msg["content"])
-
-    #     # Generate a response for the initial user message
-    #     if len(st.session_state.messages) == 1:
-    #         client = OpenAI(api_key=openai_api_key)
-    #         response = client.chat.completions.create(
-    #             model="gpt-3.5-turbo",
-    #             messages=st.session_state.messages,
-    #             max_tokens=2048,
-    #             n=1,
-    #             stop=None,
-    #             temperature=0.5
-    #         )
-
-    #         msg = response.choices[0].message.content
-    #         st.session_state.messages.append({"role": "assistant", "content": msg})
-    #         st.chat_message("assistant").write(msg)
-
-    #     # User can ask follow-up questions
-    #     if prompt := st.chat_input():
-    #         if not openai_api_key:
-    #             st.info("API key is not valid.")
-    #             st.stop()
-
-    #         client = OpenAI(api_key=openai_api_key)
-    #         st.session_state.messages.append({"role": "user", "content": prompt})
-    #         st.chat_message("user").write(prompt)
-
-    #         response = client.chat.completions.create(
-    #             model="gpt-3.5-turbo",
-    #             messages=st.session_state.messages,
-    #             max_tokens=2048,
-    #             n=1,
-    #             stop=None,
-    #             temperature=0.5
-    #         )
-
-    #         msg = response.choices[0].message.content
-    #         st.session_state.messages.append({"role": "assistant", "content": msg})
-    #         st.chat_message("assistant").write(msg)
-    st.markdown("""
+st.markdown("""
         <style>
 
             .stTabs [data-baseweb="tab-list"] {
@@ -356,7 +269,7 @@ with tab3:
                 height: 50px;
                 width: 100%;
                 white-space: pre-wrap;
-                background-color: #F1C0B9;
+                background-color: #c1d8a9;
                 border-radius: 4px 4px 0px 0px;
                 gap: 1px;
                 padding-top: 10px;
@@ -366,9 +279,12 @@ with tab3:
             }
 
             .stTabs [aria-selected="true"] {
-                background-color: #F1C0B9;
+                background-color: #c1d8a9;
                 color: black;
                 font-weight: bold; /* Added this line to make the text bold */
             }
 
         </style>""", unsafe_allow_html=True)
+
+with open("total_groceries.txt", "w") as file:
+    file.write(str(session_state.total_groceries))
